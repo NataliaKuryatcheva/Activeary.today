8bcc777f6ef307cc9df8fae72c2c9845814bca42
KakashiHatake22 <kakashi.anbu.hatake22@gmail.com>
2019-07-04 22:05:00 +0300

    changed list styles

--- frontend/src/components/Activities.js
+++ frontend/src/components/Activities.js
@@ -7,23 +7,40 @@ const purple = 'rgb(117, 147, 243)'
 const green = '#5cf761'
 const pink = 'rgb(247, 92, 171)'
 
+const showActivities = () => {}
+
 const Activities = ({ activities }) => {
   return (
     <div
       className='Activities'
       css={css`
         display: grid;
+        grid-template-areas: 'sidebar-desktop main';
         grid-template-columns: 300px auto;
         width: 100vw;
         height: 100vh;
+
+        @media (max-width: 800px) {
+          grid-template-columns: 80px auto;
+          grid-template-areas: 'sidebar-mobile main';
+        }
       `}>
       <ul
         className='List'
         css={css`
+          grid-area: sidebar-desktop;
           border-right: 1px solid black;
           height: 100%;
           text-align: left;
           list-style-type: none;
+
+          @media (max-width: 800px) {
+            display: none;
+
+            &.show {
+              display: block;
+            }
+          }
         `}>
         {activities.map((activity, index) => {
           return (
@@ -43,7 +60,71 @@ const Activities = ({ activities }) => {
           )
         })}
       </ul>
-      <div className='Detail' />
+
+      <ul
+        css={css`
+          grid-area: sidebar-mobile;
+          border-right: 1px solid black;
+          height: 100%;
+          text-align: left;
+          list-style-type: none;
+
+          @media (min-width: 800px) {
+            display: none;
+          }
+        `}>
+        <div
+          id='menuToggle'
+          css={css`
+            display: block;
+            padding: 20px 0px 0px 20px;
+          `}
+          onClick={showActivities}>
+          <span
+            css={css`
+              display: block;
+              width: 33px;
+              height: 4px;
+              margin-bottom: 5px;
+              position: relative;
+              background-color: ${pink};
+              border-radius: 3px;
+              z-index: 1;
+            `}
+          />
+          <span
+            css={css`
+              display: block;
+              width: 33px;
+              height: 4px;
+              margin-bottom: 5px;
+              position: relative;
+              background-color: ${pink};
+              border-radius: 3px;
+              z-index: 1;
+            `}
+          />
+          <span
+            css={css`
+              display: block;
+              width: 33px;
+              height: 4px;
+              margin-bottom: 5px;
+              position: relative;
+              background-color: ${pink};
+              border-radius: 3px;
+              z-index: 1;
+            `}
+          />
+        </div>
+      </ul>
+
+      <div
+        className='Detail'
+        css={css`
+          grid-area: main;
+        `}
+      />
     </div>
   )
 }
