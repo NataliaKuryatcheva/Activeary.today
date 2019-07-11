3de3ced682cc4ff675825edc8c1be62fa3ccc7eb
NataliaKuryatcheva <natalie.kur2001@gmail.com>
2019-07-11 22:16:06 +0300

    fixed butoon add in list

--- frontend/src/components/Activities/ActivitiesList.js
+++ frontend/src/components/Activities/ActivitiesList.js
@@ -13,19 +13,24 @@ const ActivitiesList = ({
   setCurrentActivity
 }) => {
   return (
-    <div>
+    <div
+      css={css`
+        grid-area: sidebar-desktop;
+        border-right: 1px solid black;
+        height: 100%;
+        text-align: left;
+
+        @media (max-width: 800px) {
+          display: ${showActivities ? 'block' : 'none'};
+        }
+
+        display: grid;
+        grid-template-rows: auto 65px;
+      `}>
       <ul
         className='List'
         css={css`
-          grid-area: sidebar-desktop;
-          border-right: 1px solid black;
-          height: 100%;
-          text-align: left;
           list-style-type: none;
-
-          @media (max-width: 800px) {
-            display: ${showActivities ? 'block' : 'none'};
-          }
         `}>
         {activities.map((activity, index) => {
           return (
@@ -50,12 +55,16 @@ const ActivitiesList = ({
         })}
       </ul>
 
-      <div>
+      <div
+        css={css`
+          border: 1px ${purpleHover} solid;
+        `}>
         <button
           onClick={() => {
             alert('HEY!')
-          }}
-        />
+          }}>
+          <span>✚</span>
+        </button>
       </div>
     </div>
   )
